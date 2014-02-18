c5e57e69c89d712a56577918579fa9a2e64b28f1
Randle Taylor <randle.taylor@gmail.com>
2014-02-17 21:32:31 -0500

    add push to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,10 @@
-from fabric.api import run, env, cd
+from fabric.api import run, env, cd, local
 
 
 env.hosts = ['randlet.com']
 
 def deploy():
+    local("git push origin master")
     venv = "source ~/venvs/randlet.com/bin/activate && "
     with cd("~/projects/randlet.com"):
         run("git pull origin master")
